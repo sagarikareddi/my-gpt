ee71ff25bc0f1290c1a9b5ffe17200c2587e8f31
Sagarika Reddi <81488244+sagarikareddi@users.noreply.github.com>
2024-02-22 22:40:41 +0530

    Update openai-config.js

--- Backend/dist/config/openai-config.js
+++ Backend/dist/config/openai-config.js
@@ -1,9 +1,11 @@
 import { Configuration } from "openai";
 export const configureOpenAI = () => {
+    console.log(process.env.OPEN_AI_SECRET);
     const config = new Configuration({
         apiKey: process.env.OPEN_AI_SECRET,
         organization: process.env.OPENAI_ORAGANIZATION_ID,
     });
+    console.log(config);
     return config;
 };
 //# sourceMappingURL=openai-config.js.map
