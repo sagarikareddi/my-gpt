edfb695ddd5833592ee0de33ec6ba5e3c5841dd5
Sagarika Reddi <81488244+sagarikareddi@users.noreply.github.com>
2024-02-22 18:51:25 +0530

    Add files via upload

---
+++ Frontend/src/components/Header.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
+
+const Header = () => {
+  const auth = useAuth();
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#435B66"
+                to="/chat"
+                text="Go To Chat"
+                textColor="Black"
+              />
+              <NavigationLink
+                bg="#435B66"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#435B66"
+                to="/login"
+                text="Login"
+                textColor="white"
+              />
+              <NavigationLink
+                bg="#435B66"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
